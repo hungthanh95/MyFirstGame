46e43722b4dd6e4e56627ae5d18c950ab7763123
LE HUNG THANH <hungthanh95@gmail.com>
2020-01-03 21:13:10 +0700

    Fisrt commit
    Fisrt commit

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdventureGame : MonoBehaviour
+{
+	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+	// Start is called before the first frame update
+
+	State state;
+	void Start()
+	{
+		state = startingState;
+		textComponent.text = state.GetStateStory();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		ManageState();
+
+	}
+
+	private void ManageState()
+	{
+		var nextState = state.GetNextState();
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			state = nextState[0];
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			state = nextState[1];
+		}
+		else
+		{
+			
+		}
+		textComponent.text = state.GetStateStory();
+	}
+}
---
+++ Assets/Scripts/NumberMagic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberMagic : MonoBehaviour
+{
+	int maxNumber = 1000;
+	int minNumber = 1;
+	int guessNumber = 500;
+	// Start is called before the first frame update
+	void Start()
+    {
+		Debug.Log("Welcome to Magic Number!!!!!!!!!!!!");
+		Debug.Log("Please pick up your numbers, don't tell me what is it....");
+		Debug.Log("The highest number you can pick is: " + maxNumber);
+		Debug.Log("The lowest number you can pick is: " + minNumber);
+		Debug.Log("Tell me you pick number higher or lower than 500.");
+		Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+		maxNumber = maxNumber + 1;
+	}
+	int CalGuessNum(int min, int max)
+	{
+		return (int)((min + max) / 2);
+	}
+	void LetGuess(ref int num)
+	{
+		num = guessNumber;
+		guessNumber = CalGuessNum(minNumber, maxNumber);
+		Debug.Log("Your guess number is " + guessNumber);
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("Up Arrow key was pressed.");
+			LetGuess(ref minNumber);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Debug.Log("Down Arrow key was pressed.");
+			LetGuess(ref maxNumber);
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("The guess number is correct.");
+		}
+
+	}
+}
